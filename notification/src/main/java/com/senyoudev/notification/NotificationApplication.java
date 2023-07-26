f7a269bd9c45194b0291cde76f7eb95a22a29d43
senyoudev <younesmeskafe2018@gmail.com>
2023-07-26 21:30:14 +0100

    preparing for deploying microservices into k8s

--- notification/src/main/java/com/senyoudev/notification/NotificationApplication.java
+++ notification/src/main/java/com/senyoudev/notification/NotificationApplication.java
@@ -3,6 +3,8 @@ package com.senyoudev.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -10,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 "com.senyoudev.notification",
         }
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
